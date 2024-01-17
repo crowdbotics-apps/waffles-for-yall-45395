95bf8ad9fee7a83879c43873525a60060359f2a5
Crowdbotics <team@crowdbotics.com>
2024-01-17 02:11:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "waffles-for-yall-45395",
+  url: "https://waffles-for-yall-45395.botics.co",
+  api: "https://waffles-for-yall-45395.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
